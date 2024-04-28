6aa6076a9fa44b0f5758b018873d4f2cc96494c3
roadams <dr2@free.fr>
2024-04-28 19:08:14 +0200

    adds reply

--- Main.java
+++ Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello les amis, ça roule, tout va bien ? Pour moi c'est cool !");
+        System.out.println("Oui ça va, merci et toi ?");
     }
 }
